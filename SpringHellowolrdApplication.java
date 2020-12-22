298966454982c619d7d2b7b19ac1f01a8fce9fb9
Nathiya-1 <mnk19980415@gmail.com>
2020-12-21 21:45:04 -0800

    sec commit

---
+++ SpringHellowolrdApplication.java
@@ -0,0 +1,14 @@
+package com.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHellowolrdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHellowolrdApplication.class, args);
+		System.out.println("success");
+	}
+
+}
